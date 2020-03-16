814f6d9ee35b85f08c60f202f60cf2d2e38d8e4e
shahzaintariq <shahzaintariq.crpyto@gmail.com>
2020-03-16 03:28:02 -0700

    updated

--- contracts/todolist.sol
+++ contracts/todolist.sol
@@ -7,36 +7,44 @@ contract todolist{
         string name;
         address Address;
     }
-
+    struct tasks{
+        string value;
+    }
+    tasks[] task;
     userData[] usersData;
 
+
     mapping(address => uint) paymentDetails;
-    mapping(address => string[]) data;
+    //mapping(address => string[]) data;
 
 
     //user should have 0.5 ether to open account on todolist
     function registerUser(uint _id, string memory _name) public payable returns(bool){
-        require(msg.value >= 0.5 ether,"You Don't Have Enough Fund");
+        require(msg.value >= 0.5 ether,"You should have minimun of 0.5 ether");
         paymentDetails[msg.sender] = msg.value;
         usersData.push(userData(_id,_name,msg.sender));
         return true;
-    } 
+    }
 
     function create(string memory _data) public returns(bool){
         require(paymentDetails[msg.sender] >= 0.5 ether,"register yourself first");
-        data[msg.sender] = [_data];
+        task.push(tasks(_data));
         return true;
     }
 
     function read() public view returns(string[] memory){
         require(paymentDetails[msg.sender] >= 0.5 ether,"register yourself first");
-        return data[msg.sender];
+
+        string[] memory item = new string[](task.length);
+        for(uint i=0; i<task.length; i++){
+            item[i] = task[i].value;
+        }
+        return item;
     }
 
-    function delete() public returns(bool){
-        require(paymentDetails[msg.sender] >= 0.5 ether,"register yourself first");
-        
-    }
+    // function delete() public returns(bool){
+    //     require(paymentDetails[msg.sender] >= 0.5 ether,"register yourself first");
+    // }
 
 
 
